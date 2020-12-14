b369cc37cc6a6820cce73938805363cfc24425a3
KiKiKi <chaika.design@gmail.com>
2020-12-14 20:41:33 +0900

    :zap: Singleton

--- api/api.py
+++ api/api.py
@@ -7,10 +7,22 @@ from scraping import get_web_data
 
 api = Blueprint('api', __name__)
 
+scraping_data = None
+
+
+def get_data():
+    global scraping_data
+
+    if scraping_data != None:
+        return scraping_data
+    else:
+        scraping_data = get_web_data()
+        return scraping_data
+
 
 @api.route('/api/get_random', methods=['GET'])
 def get_one():
-    data = get_web_data()
+    data = get_data()
     shuffle(data)
     return jsonify({
         'status': 'ok',
