3618b82910225e2b911fbb62dfaa9fe832d4ca17
KiKiKi <chaika.design@gmail.com>
2020-12-14 20:56:26 +0900

    :rabbit: Implement get story data by story number.

--- api/api.py
+++ api/api.py
@@ -20,11 +20,39 @@ def get_data():
         return scraping_data
 
 
-@api.route('/api/get_random', methods=['GET'])
+@api.route('/api/random', methods=['GET'])
 def get_one():
     data = get_data()
     shuffle(data)
     return jsonify({
         'status': 'ok',
         'data': data[0]
+    })
+
+
+@api.route('/api/stories/<int:story_no>', methods=['GET'])
+def get_one_by_index(story_no):
+    no = int(story_no)
+
+    if not no:
+        bad_request('NO DATA!')
+
+    res = None
+    data = get_data()
+    max = len(data)
+
+    if no > max:
+        return jsonify({
+            'status': 'ok',
+            'data': res
+        })
+
+    for story in data:
+        if story[0] == no:
+            res = story
+            break
+
+    return jsonify({
+        'status': 'ok',
+        'data': res
     })
